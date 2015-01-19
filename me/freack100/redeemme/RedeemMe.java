43f74ff007884dbb93bb39153c9ccdaec3608d03
TheFreakLord <freack1208@gmail.com>
2015-01-19 09:55:41 +0100

    Added license.

--- me/freack100/redeemme/RedeemMe.java
+++ me/freack100/redeemme/RedeemMe.java
@@ -1,3 +1,12 @@
+/*
+ *
+ *  * Copyright © 2014-2015 Paul Waslowski <freack1208@gmail.com>
+ *  * This work is free. You can redistribute it and/or modify it under the
+ *  * terms of the Do What The Fuck You Want To Public License, Version 2,
+ *  * as published by Sam Hocevar. See the LICENSE file for more details.
+ *
+ */
+
 package me.freack100.redeemme;
 
 import me.freack100.redeemme.command.*;
